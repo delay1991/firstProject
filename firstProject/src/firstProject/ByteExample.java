e2ba5025f06422f0373e336d865f5323daea40e9
Admin <Admin@DESKTOP-5KUCMGU>
2021-06-23 16:32:53 +0900

    마지막

---
+++ firstProject/src/firstProject/ByteExample.java
@@ -0,0 +1,17 @@
+package firstProject;
+
+public class ByteExample {
+	public static void main(String[] args) {
+		int numb1 = 125; // -128 ~ 127
+		
+		 ++ numb1 ;
+		 ++ numb1 ;
+		 ++ numb1 ;
+		System.out.println(numb1);
+
+		int numb2 = 13;
+		int numb3 = 5;
+		int rem = numb2 % numb3;
+		System.out.println( numb3 + "로 나눈 나머지:" + rem);
+}
+}
