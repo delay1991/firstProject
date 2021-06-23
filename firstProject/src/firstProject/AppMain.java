1bbba8f208960367c407e763e27a4ad9e55b96bd
Admin <Admin@DESKTOP-5KUCMGU>
2021-06-23 14:35:05 +0900

    첫번째

---
+++ firstProject/src/firstProject/AppMain.java
@@ -0,0 +1,12 @@
+package firstProject;
+
+public class AppMain {
+	//주석문: 프로그램실행과 관계없이 실행.
+	   public static void main(String[] args) {
+	      int age = 25;
+	      String name = "김지연";
+	      System.out.println("내 이름는 :" + name);
+	      System.out.println("내 나이는 :" + age);
+	   }
+
+}
---
+++ firstProject/src/firstProject/NumberExample.java
@@ -0,0 +1,12 @@
+package firstProject;
+
+public class NumberExample {
+	 public static void main(String[] args) {
+	      byte math = 100; // -128 ~ 127 
+	      byte eng = 90;
+	      
+	      int sum = math + eng;
+	      System.out.println("두점수의 합 :" + sum );
+	   }
+
+}
