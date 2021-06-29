369c4c53d806f7a57bcce8e1d0adebfbce969655
Hello <Hello@192.168.0.5>
2021-06-30 02:22:20 +0900

    ex2

--- firstProject/src/referance/ArrayExample2.java
+++ firstProject/src/referance/ArrayExample2.java
@@ -12,22 +12,36 @@ public class ArrayExample2 {
 
 		if (num1 > num2) {
 			if (num1 > num3) {
-				intAry[0] = num1; 
-				if(num2>num3) { //1>2>3
-					intAry[1]=num2;
-				}else if(num2<num3) { //1>3>2
-					intAry[1]=num3;
+				intAry[0] = num1;
+				if (num2 > num3) { // 1>2>3
+					intAry[1] = num2;
+					intAry[2] = num3;
+				} else if (num2 < num3) { // 1>3>2
+					intAry[1] = num3;
+					intAry[2] = num2;
 				}
-			} else if(num1<num3) { //3>1>2
-				intAry[0]= num3;
 			}
-		}else if(num2 > num1) {
-			if(num2 > num3) { 
-				intAry[1] = num2;
-				if(num1>num3){//2>1>3
-				}
-				}
+		} if (num1 < num2) {
+			intAry[0] = num2;
+			if (num1 > num3) { // 2>1>3
+				intAry[1] = num1;
+				intAry[2] = num3;
+			} else if (num1 < num3) {// 2>3>1
+				intAry[1] = num3;
+				intAry[2] = num1;
+			}
 		}
+		if (num3 > num1) {
+			intAry[0] = num3; 
+			if (num1 > num2) { //3>1>2
+				intAry[1] = num1;
+				intAry[2] = num2;
+			} else if (num1 < num2) { //3>2>1
+				intAry[1] = num2;
+				intAry[2] = num1;
 
-	}
-}
+			}
+		}
+		System.out.println(intAry[0] + "," + intAry[1] + "," + intAry[2]);
+	}// main
+}// class
