79f1884259d52a3d4aebe1e08d070f08f24648ab
Hello <Hello@192.168.0.5>
2021-07-05 00:27:47 +0900

    7/5 과제

---
+++ 20210630/src/co/yedam/exercise/Account.java
@@ -0,0 +1,35 @@
+package co.yedam.exercise;
+
+public class Account {
+	private String ano;
+	private String owner;
+	private int balance;
+	public String getAno() {
+		return ano;
+		
+		
+	}
+	public Account(String ano, String owner, int balance) {
+		super();
+		this.ano = ano;
+		this.owner = owner;
+		this.balance = balance;
+	}
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	
+}
